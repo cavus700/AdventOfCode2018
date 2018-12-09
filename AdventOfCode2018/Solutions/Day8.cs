6c0272296d6a8838ed92d7137bb7680dbcf40a77
Robin.Hubbig <robin.hubbig@udo.edu>
2018-12-09 23:35:42 +0100

    Solved Day 8 and 9.1

---
+++ AdventOfCode2018/Solutions/Day8.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2018
+{
+    class Day8 : AoCDay
+    {
+        public override void startA()
+        {
+            var input = readInput<List<int>>();
+            Console.WriteLine($"Solution for Day8.1 is {SumMetadata(ref input, 0)}");
+        }
+
+        private int SumMetadata(ref List<int> input, int iter)
+        {
+            int numChilds = input[iter];
+            int result = 0;
+            for(int iChild = 0; iChild < numChilds; ++iChild)
+            {
+                result += SumMetadata(ref input, iter + 2);
+            }
+
+            for(int iMeta = 0; iMeta < input[iter+1]; ++iMeta)
+            {
+                result += input[iMeta + iter + 2];
+            }
+            input.RemoveRange(iter, input[iter + 1] + 2);
+            return result;
+        }
+
+        public override void startB()
+        {
+            var input = readInput<List<int>>();
+            Tree tree = BuildMetadataTree(ref input, 0);
+
+            Console.WriteLine($"Solution for Day8.2 is {GetValue(ref tree)}");
+        }
+
+        public int GetValue(ref Tree tree)
+        {
+            int add = 0;
+            if (tree.Children.Count == 0)
+            {
+                add = tree.Metadata.Sum();
+                return add;
+            }
+
+            foreach(var meta in tree.Metadata)
+            {
+                if(meta-1 >= 0 && meta-1 < tree.Children.Count)
+                {
+                    Tree child = tree.Children[meta - 1];
+                    add += GetValue(ref child);
+                }
+            }
+            return add;            
+        }
+
+        private Tree BuildMetadataTree(ref List<int> input, int iter)
+        {
+            Tree currTree = new Tree();
+            currTree.MetadataEntries = input[iter + 1];
+
+            int numChilds = input[iter];
+            for (int iChild = 0; iChild < numChilds; ++iChild)
+            {
+                currTree.Children.Add(BuildMetadataTree(ref input, iter + 2));
+            }
+
+            for (int iMeta = 0; iMeta < input[iter + 1]; ++iMeta)
+            {
+                currTree.Metadata.Add(input[iMeta + iter + 2]);
+            }
+
+            input.RemoveRange(iter, input[iter + 1] + 2);
+            return currTree;
+        }        
+
+        internal class Tree
+        {
+            public int MetadataEntries;
+            public List<int> Metadata;
+            public List<Tree> Children;
+
+            public Tree()
+            {
+                Metadata = new List<int>();
+                Children = new List<Tree>();
+            }
+        }
+
+        protected override T readInput<T>()
+        {
+            var input = File.ReadAllLines("../../Input/Day8.txt")[0].Split(' ').Select(int.Parse).ToList();
+            return (T)Convert.ChangeType(input, typeof(T));
+        }
+    }
+}
---
+++ AdventOfCode2018/Solutions/Day9.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2018
+{
+    class Day9 : AoCDay
+    {
+        public override void startA()
+        {
+            Console.WriteLine($"Solution for Day9.1 is {PlayMarbles(458, 71307)}");
+        }
+
+        public override void startB()
+        {
+            Console.WriteLine($"Solution for Day9.2 is {PlayMarbles(458, 71307 * 100)}");
+        }
+
+        private ulong PlayMarbles(int numPlayers, int maxMarbles)
+        {
+            ulong[] scores = Enumerable.Repeat(0uL, numPlayers).ToArray();
+            int currPlayer = 0;
+            int currMarbleIdx = 1;
+
+            List<int> marbles = new List<int>();
+            marbles.Add(0);
+            marbles.Add(2);
+            marbles.Add(1);
+
+            for (int marbleCounter = 3; marbleCounter <= maxMarbles; ++marbleCounter)
+            {
+                if (marbleCounter % 23 == 0)
+                {
+                    int rmIdx = ((currMarbleIdx + marbles.Count) - 7) % marbles.Count;
+                    scores[currPlayer] += Convert.ToUInt64(marbleCounter);
+                    scores[currPlayer] += Convert.ToUInt64(marbles[rmIdx]);
+                    marbles.RemoveAt(rmIdx);
+                    currMarbleIdx = rmIdx % marbles.Count;
+                }
+                else
+                {
+                    currMarbleIdx = (currMarbleIdx + 2) % marbles.Count;
+                    marbles.Insert(currMarbleIdx, marbleCounter);
+                }
+                currPlayer = (currPlayer + 1) % numPlayers;
+            }
+
+            return scores.Max();
+        }
+
+        protected override T readInput<T>()
+        {
+            var input = File.ReadAllLines("../../Input/Day9.txt");
+            return (T)Convert.ChangeType(input, typeof(T));
+        }
+    }
+}
